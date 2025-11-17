2419579e3d451cdc8c5527804e0d7003f0350c50
Tian R <78232090+tianrich@users.noreply.github.com>
2025-11-17 12:56:02 +0000

    Update choco.py
    More links added

--- choco.py
+++ choco.py
@@ -17,7 +17,11 @@ urls = [
     "https://secretsanta.cadbury.co.uk/code/d1f53ad8-fa57-49fb-b7f0-fb110b8a812e",
     "https://secretsanta.cadbury.co.uk/secretsanta/CfDJ8J-4L2JLEjBLsbt_V274SAuz6j9GxI6MJ_SlwNHWwywFwDKpF0URDctiE9jnlfBSzWsTjAX7Uxqvg2ueEL4zmPI2JnJLKPLYSCHRe5JKCi5fusZ0me2pcm_bGHuZoJv3RZqakBvfWUSU?ar=False&codeId=CfDJ8J-4L2JLEjBLsbt_V274SAs5JMDu-XMsFN2RPxMMOAE9Kmnt7wDQ9s7hCPzvaKtPmnwtuulttWZMb0BhBZqriXME-wn0XpKXSDEhM2md50_JOEq-HwKnn9FTMJFYINsgeJxHYoD72B1O&utm_source=QR&utm_medium=QR_Code",
     "https://secretsanta.cadbury.co.uk/code/982cb88e-5764-462c-6f75-08de1d992242",
-    "https://secretsanta.cadbury.co.uk/secretsanta/CfDJ8J-4L2JLEjBLsbt_V274SAthTLbuqJGombFSxRpxu2Jbty5idkvMITls-5-r-uwtJwWctvACr4U8b44sV1oCw8wYV0nBF4EBAr3g18GIcPobbXPOoSaqt9dZFDxerFpctcrgm0XOO-DJ"
+    "https://secretsanta.cadbury.co.uk/secretsanta/CfDJ8J-4L2JLEjBLsbt_V274SAthTLbuqJGombFSxRpxu2Jbty5idkvMITls-5-r-uwtJwWctvACr4U8b44sV1oCw8wYV0nBF4EBAr3g18GIcPobbXPOoSaqt9dZFDxerFpctcrgm0XOO-DJ",
+    "https://secretsanta.cadbury.co.uk/code/63699f0c-b271-4055-85e2-08dcfdae2585",
+    "https://secretsanta.cadbury.co.uk/code/631e209e-4a2b-4cc5-e280-08dcfdae24ce",
+    "https://secretsanta.cadbury.co.uk/code/61d71f43-30ea-4685-8c1a-08dcfcd4f6a7",
+    "https://secretsanta.cadbury.co.uk/code/7c019550-fa88-4260-94b0-ffaae170fbfd",
 ]
 
 def check_redirect(url): 
